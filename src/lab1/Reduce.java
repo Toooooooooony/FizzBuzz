a2de325b1a17c43624ab0047871f1f62f8b77f65
Tony Wu <tongzhou.wu@mail.utoronto.ca>
2026-09-15 19:50:15 -0400

    New Java File Implementing Reduce Function

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -1,4 +1,18 @@
 package lab1;
 
 public class Reduce {
+    public static void main(String[] args) {
+        int cnt = 0;
+        int n = 100;
+        while(n != 0) {
+            if(n % 2 == 0) {
+                n /= 2;
+            }
+            else {
+                n--;
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
 }
