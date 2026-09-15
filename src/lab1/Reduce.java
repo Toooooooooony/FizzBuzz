5f54e105235ce4191613515fcf00590c5e68afb7
Tony Wu <tongzhou.wu@mail.utoronto.ca>
2026-09-15 19:56:09 -0400

    Latest Updates

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -2,8 +2,12 @@ package lab1;
 
 public class Reduce {
     public static void main(String[] args) {
+//        int cnt = reduce();
+//        System.out.println(cnt);
+    }
+
+    static int reduce(int n) {
         int cnt = 0;
-        int n = 100;
         while(n != 0) {
             if(n % 2 == 0) {
                 n /= 2;
@@ -13,6 +17,6 @@ public class Reduce {
             }
             cnt++;
         }
-        System.out.println(cnt);
+        return cnt;
     }
 }
